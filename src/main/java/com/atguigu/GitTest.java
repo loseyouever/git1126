ed46432756a3e59808de2a768e009c097385daa9
wwl <18715112302@163.com>
2022-04-14 19:10:49 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -8,6 +8,7 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("gittest");
+        System.out.println("gittest-hot-fix");
     }
 }
 

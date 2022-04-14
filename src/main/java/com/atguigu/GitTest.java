30ebce2bba494f5b67cdb7e64023ba2abed2bf36
wwl <18715112302@163.com>
2022-04-14 18:37:43 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @Author:wwl
+ * @Email:wwl1536414718@163.com
+ * @Datetime:2022年04月14日18:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("gittest");
+    }
+}
+
